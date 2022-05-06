cfb8b28bf5b69bb11e32a8254d4ff023acde2072
Cidar de Alencar <cidardealencar@LAPTOP-FGLAEUQ5.localdomain>
2022-05-05 20:06:50 -0400

    [add] Vector Prueba

---
+++ Taller/time-mejora/src/CardAPI/ShadowList.js
@@ -0,0 +1,22 @@
+import work from'../assets/icon-work.svg'
+import play from'../assets/icon-play.svg'
+
+const ShadowList =[
+    {
+        Type:'Work',
+        Houres:'32',
+        hrsweek:'36',
+        img:{work},
+        theme:'Work '
+    },
+    {
+        Type:'Play', 
+        Houres:'10',
+        hrsweek:'8',
+        img:{play},
+        theme:'Play '
+    }
+
+
+]
+export default ShadowList;
--- Taller/time-mejora/src/components/App.js
+++ Taller/time-mejora/src/components/App.js
@@ -1,16 +1,28 @@
 //eliminar logo.svg
 import '../styles/App.css';
 import {Shadow} from './Shadow';
-import work from'../assets/icon-work.svg'
-import play from'../assets/icon-play.svg'
+import ShadowList from '../CardAPI/ShadowList'
+//import work from'../assets/icon-work.svg'
+//import play from'../assets/icon-play.svg'
 
 function App() {
   //eliminar contenido del return
   // const theme='Shadow '+  props.theme
   return (
     <>
-    <Shadow Type='Work' Houres='32' hrsweek='36'img={work} theme='Work '/>
-    <Shadow Type='Play' Houres='10'hrsweek='8'img={play} theme='Play '/>
+    {
+        ShadowList.map(
+          (card)=>
+          <Shadow
+          Type={card.Type}
+          Houres={card.Houres}
+          hrsweek={card.hrsweek}
+          img={card.img}
+          theme={card.theme}
+          />
+       )
+      }
+    {/* <Shadow/> */}
     </>
   );
 }
